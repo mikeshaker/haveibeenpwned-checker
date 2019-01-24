3867304d0c1840fe471cb554cc6679139c69bc04
Mike Shaker <mike.shaker@live.com>
2019-01-24 12:51:44 -0500

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pwnedPassword
-Pwned Passwords check passwords if they have previously been exposed in data breaches. based on https://haveibeenpwned.com
+Pwned Passwords check passwords if they have previously been exposed in data breaches.
+Using PwnedPasswords API by Troy Hunt (haveibeenpwned.com).
+
+## Installation
+
+```sh
+npm i haveibeenpwned-checker
+```
+
+## Usage
+```js
+const isPasswordPwned = require('haveibeenpwned-checker');
+
+
+// password : password string to check//
+// callback: callback method 
+//timeout -(optional) by default it's 3000 ms integer containing the number of milliseconds to wait for a server to send response headers (and start the response body) before aborting the request. 
+
+isPasswordPwned('password', this.passwordPwnedCallback, 1000);
+
+
+function passwordPwnedCallback (e){
+   console.log(e);
+   //{ error: '', failed: false, count: 3645804 }
+}
--- index.js
+++ index.js
@@ -1,6 +1,50 @@
-import request from 'request';
 
+// const crypto = require('crypto')
+var request = require('request');
+const crypto = require('crypto');
 
-exports.isPwned = function() {
-    console.log("This is a message from the demo package");
+const PREFIX_LENGTH = 5
+const API_URL = 'https://api.pwnedpasswords.com/range/'
+
+function isPasswordPwned (password, cb, timeout = 3000) {
+  const hasCallback = typeof cb === 'function'
+
+  if (typeof password !== 'string') {
+    const err = new Error('Input password must be a string.')
+    return hasCallback ? cb(err) : Promise.reject(err)
   }
+  const hashedPassword = hash(password);
+  const hashedPasswordPrefix = hashedPassword.substr(0, PREFIX_LENGTH); 
+  const hashedPasswordSuffix = hashedPassword.substr(PREFIX_LENGTH);
+
+  var result = {
+    error: "",
+    failed : false,
+    count  :  0,
+    pwned : false
+  };
+  request(API_URL + hashedPasswordPrefix, { timeout: timeout }, (err, res, body) => {
+    if (err) {
+        result.error = err;
+        result.failed = true;
+        
+      return cb(result);
+    }
+    const pwnedCount = body
+        .split('\n')
+        .map(line => line.split(':'))
+        .filter(filtered => filtered[0].toLowerCase() === hashedPasswordSuffix)
+        .map(mapped => Number(mapped[1]))
+        .shift() || 0;
+        result.count = pwnedCount;
+        result.pwned = true;
+    return cb(result);
+ });
+}
+
+function hash(password) {
+    const shaSum = crypto.createHash('sha1')
+    shaSum.update(password)
+    return shaSum.digest('hex')
+}
+module.exports = isPasswordPwned
