d1c11208d0505852d74125c526fc51815ded5125
Mike Shaker <mike.shaker@live.com>
2019-01-25 15:35:37 -0500

    clean up

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const request = require('request');
 const crypto = require('crypto');
 
-const PREFIX_LENGTH = 5
-const API_URL = 'https://api.pwnedpasswords.com/range/'
+const PREFIX_LENGTH = 5;
+const API_URL = 'https://api.pwnedpasswords.com/range/';
 
 function isPasswordPwned(password, cb, timeout = 3000) {
-  const hasCallback = typeof cb === 'function'
+  const hasCallback = typeof cb === 'function';
 
   if (typeof password !== 'string') {
-    const err = new Error('Input password must be a string.')
-    return hasCallback ? cb(err) : Promise.reject(err)
+    const err = new Error('Input password must be a string.');
+    return hasCallback ? cb(err) : Promise.reject(err);
   }
   const hashedPassword = hash(password);
   const hashedPasswordPrefix = hashedPassword.substr(0, PREFIX_LENGTH);
@@ -30,9 +30,9 @@ function isPasswordPwned(password, cb, timeout = 3000) {
     }
     const pwnedCount = body
       .split('\n')
-      .map(line => line.split(':'))
-      .filter(filtered => filtered[0].toLowerCase() === hashedPasswordSuffix)
-      .map(mapped => Number(mapped[1]))
+      .map((line) => line.split(':'))
+      .filter((filtered) => filtered[0].toLowerCase() === hashedPasswordSuffix)
+      .map((mapped) => Number(mapped[1]))
       .shift() || 0;
     result.count = pwnedCount;
     result.pwned = pwnedCount > 0;
@@ -41,8 +41,8 @@ function isPasswordPwned(password, cb, timeout = 3000) {
 }
 
 function hash(password) {
-  const shaSum = crypto.createHash('sha1')
-  shaSum.update(password)
-  return shaSum.digest('hex')
+  const shaSum = crypto.createHash('sha1');
+  shaSum.update(password);
+  return shaSum.digest('hex');
 }
-module.exports = isPasswordPwned
+module.exports = isPasswordPwned;
